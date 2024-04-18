79819fd001c47baa1dc750c85ecf3b07aba51007
Thiago Ferreira <thiagoferreira@tuta.io>
2024-04-18 19:31:12 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+dotfiles
+---
+
+_Enter the void_
+
+This is my daily driver setup.
+
+- Colors by [pywal](https://github.com/dylanaraps/pywal/).
+- Fetch is [fet.sh](https://github.com/eepykate/fet.sh).
+
+![screenshot](screenshot1.png)
---
+++ wal/templates/colors-st.h
@@ -0,0 +1,38 @@
+static const char *colorname[] = {{
+        /* 8 normal colors */
+        "{color0}",
+        "{color1}",
+        "{color2}",
+        "{color3}",
+        "{color4}",
+        "{color5}",
+        "{color6}",
+        "{color7}",
+
+        /* 8 bright colors */
+        "{color8}",
+        "{color9}",
+        "{color10}",
+        "{color11}",
+        "{color12}",
+        "{color13}",
+        "{color14}",
+        "{color15}",
+
+        [255] = 0,
+
+        /* more colors can be added after 255 to use with DefaultXX */
+        "{background}",
+        "{foreground}",
+        "{cursor}",
+}};
+
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultcs = 258;
+unsigned int defaultrcs = 258;
